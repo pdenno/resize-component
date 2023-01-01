0cc17d1fcefde81ed52973fcae0a4316f3a3fbe4
Peter Denno <podenno@gmail.com>
2023-01-01 14:20:34 -0500

    More cleanup

--- src/deps.cljs
+++
@@ -1,8 +0,0 @@
-{:foreign-libs
- [{:file           "x-mock.js"
-   :provides       ["@mui/x-data-grid"
-                    "@mui/x-date-pickers/DatePicker"
-                    "@mui/x-date-pickers/LocalizationProvider"]
-   :global-exports {"@mui/x-data-grid"                         MuiXDataGrid
-                    "@mui/x-date-pickers/DatePicker"           MuiXDatePickersDatePicker
-                    "@mui/x-date-pickers/LocalizationProvider" MuiXDatePickersLocalizationProvider}}]}
--- src/x-mock.js
+++
@@ -1,9 +0,0 @@
-var MuiXDataGrid = {
-  DataGrid: function() { return "Data Grid is not available with CLJSJS deps"; }
-};
-var MuiXDatePickersDatePicker = {
-  DatePicker: function() { return "Date Picker is not available with CLJSJS deps"; }
-};
-var MuiXDatePickersLocalizationProvider = {
-  LocalizationProvider: function(props) { return props.children; }
-};
