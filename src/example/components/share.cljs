5b2874869c49c876b41164fa59ff1d5e11044361
Peter Denno <podenno@gmail.com>
2023-01-02 16:58:30 -0500

    refactored to separate share.cljs component

---
+++ src/example/components/share.cljs
@@ -0,0 +1,90 @@
+(ns example.components.share
+  (:require
+   ["@mui/material/Stack$default" :as Stack]
+   ["@mui/material/Divider$default" :as Divider]
+   ["@mui/material/Box$default" :as MuiBox]
+   [applied-science.js-interop :as j]
+   [helix.core :refer [defnc $]]
+   [helix.hooks :as hooks]))
+
+(defnc ShareUpDown [{:keys [up down]}]
+  {:helix/features {:check-invalid-hooks-usage true}}
+  (let [[up-height set-up-height] (hooks/use-state {:size "50%"})
+        [dn-height set-dn-height] (hooks/use-state {:size "50%"})
+        u-ref (hooks/use-ref nil)
+        d-ref (hooks/use-ref nil)
+        mouse-down? (atom false)] ; ToDo: Why is this necessary? (It is necessary.)
+    (letfn [(do-drag [e]
+              (when @mouse-down?
+                (when-let [ubox (j/get u-ref :current)]
+                  (when-let [dbox (j/get d-ref :current)]
+                    (let [ubound (j/get (.getBoundingClientRect ubox) :top)
+                          dbound (j/get (.getBoundingClientRect dbox) :bottom)
+                          mouse-y (j/get e .-clientY)]
+                      (when (<  ubound mouse-y dbound)
+                        (let [upct (int (* 100 (- 1.0 (/ (- dbound mouse-y) (- dbound ubound)))))]
+                          (set-up-height {:size (str upct "%")})
+                          (set-dn-height {:size (str (- 100 upct) "%")}))))))))
+            (start-drag [_e]
+              (reset! mouse-down? true)
+              (js/document.addEventListener "mouseup"   stop-drag)
+              (js/document.addEventListener "mousemove" do-drag))
+            (stop-drag []
+              (reset! mouse-down? false)
+              (js/document.removeEventListener "mouseup"   stop-drag)
+              (js/document.removeEventListener "mousemove" do-drag))]
+      ($ Stack
+         {:direction "column"
+          :display   "flex"
+          :height    "100%"
+          :width     "100%"
+          :spacing   0
+          :divider ($ Divider {:variant "active-horiz"
+                               :height 5
+                               :onMouseDown start-drag
+                               :onMouseMove do-drag
+                               :onMouseUp   stop-drag
+                               :color "black"})}
+         ($ MuiBox {:ref u-ref :height (:size up-height)} up)
+         ($ MuiBox {:ref d-ref :height (:size dn-height)} down)))))
+
+(defnc ShareLeftRight [{:keys [left right]}]
+  {:helix/features {:check-invalid-hooks-usage true}}
+  (let [[lwidth set-lwidth] (hooks/use-state {:size "50%"})
+        [rwidth set-rwidth] (hooks/use-state {:size "50%"})
+        l-ref (hooks/use-ref nil)
+        r-ref (hooks/use-ref nil)
+        mouse-down? (atom false)] ; ToDo: Why is this necessary? (It is necessary.)
+    (letfn [(do-drag [e]
+              (when @mouse-down?
+                (when-let [rbox (j/get r-ref :current)]
+                  (when-let [lbox (j/get l-ref :current)]
+                    (let [lbound (j/get (.getBoundingClientRect lbox) :left)
+                          rbound (j/get (.getBoundingClientRect rbox) :right)
+                          mouse-x (j/get e .-clientX)]
+                      (when (<  lbound mouse-x rbound)
+                        (let [lpct (int (* 100 (/ (- mouse-x lbound) (- rbound lbound))))]
+                          (set-lwidth {:size (str lpct "%")})
+                          (set-rwidth {:size (str (- 100 lpct) "%")}))))))))
+            (start-drag [_e]
+              (reset! mouse-down? true)
+              (js/document.addEventListener "mouseup"   stop-drag)
+              (js/document.addEventListener "mousemove" do-drag))
+            (stop-drag []
+              (reset! mouse-down? false)
+              (js/document.removeEventListener "mouseup"   stop-drag)
+              (js/document.removeEventListener "mousemove" do-drag))]
+      ($ Stack
+         {:direction "row"
+          :display   "flex"
+          :height    300 #_"100%"   ; Use of mouse-down? helped here?!?
+          :width     "100%"   ; This (e.g. 300) can be used to limit width.
+          :spacing   0
+          :divider ($ Divider {:variant "active-vert"
+                               :width 5
+                               :onMouseDown start-drag
+                               :onMouseMove do-drag
+                               :onMouseUp   stop-drag
+                               :color "black"})}
+         ($ MuiBox {:ref l-ref :width (:size lwidth)} left)
+         ($ MuiBox {:ref r-ref :width (:size rwidth)} right)))))
--- src/example/core.cljs
+++ src/example/core.cljs
@@ -3,14 +3,12 @@
    ["@mui/material/Typography$default" :as Typography]
    ["@mui/material/TextField$default" :as TextField]
    ["@mui/material/Stack$default" :as Stack]
-   ["@mui/material/Divider$default" :as Divider]
-   ["@mui/material/Box$default" :as MuiBox]
    ["@mui/material/styles" :as styles]
    ;["@mui/material/CssBaseline" :as CssBaseline]
    ["@mui/material/colors" :as colors]
    [applied-science.js-interop :as j]
+   [example.components.share :refer [ShareUpDown ShareLeftRight]]
    [helix.core :refer [defnc $ <>]]
-   [helix.hooks :as hooks]
    [helix.dom :as d]
    ["react-dom/client" :as react-dom]))
 
@@ -19,43 +17,12 @@
    (j/lit {#_#_:palette {:primary   colors/yellow
                          :secondary colors/green}
            :components {:MuiDivider
-                        {:variants [{:props {:variant "active" }
-                                     :style {:cursor "ew-resize"}}]}}})))
-
-(defnc LeftRightShare [{:keys [left right]}]
-  {:helix/features {:check-invalid-hooks-usage true}}
-  (let [parent-width  (.-innerWidth  js/window)
-        parent-height (.-innerHeight js/window)
-        [lwidth set-lwidth] (hooks/use-state {:size "50%"})
-        [rwidth set-rwidth] (hooks/use-state {:size "50%"})
-        mouse-down? (atom false)] ; ToDo: Why is this necessary? (It is necessary.)
-    (letfn [(do-drag [e]
-              (when @mouse-down?
-                (let [mouse-x (j/get e .-clientX)]
-                  (set-lwidth {:size mouse-x})
-                  (set-rwidth {:size (- parent-width mouse-x)}))))
-            (start-drag [_e]
-              (reset! mouse-down? true)
-              (js/document.addEventListener "mouseup"   stop-drag)
-              (js/document.addEventListener "mousemove" do-drag))
-            (stop-drag []
-              (reset! mouse-down? false)
-              (js/document.removeEventListener "mouseup"   stop-drag)
-              (js/document.removeEventListener "mousemove" do-drag))]
-      ($ Stack
-         {:direction "row"
-          :display   "flex"
-          :height    parent-height ; Use of mouse-down? helped here?!?
-          :width     "100%" ; This (e.g. 300) can be used to limit width.
-          :spacing   0
-          :divider ($ Divider {:variant "active"
-                               :width 5
-                               :onMouseDown start-drag
-                               :onMouseMove do-drag
-                               :onMouseUp   stop-drag
-                               :color "black"})}
-         ($ MuiBox {:width (:size lwidth) :backgroundColor "purple"} left)
-         ($ MuiBox {:width (:size rwidth) :backgroundColor "blue"} right)))))
+                        {:variants [{:props {:variant "active-vert" } ; vertical divider of horizontal layout
+                                     :style {:cursor "ew-resize"
+                                             :width 5}}
+                                    {:props {:variant "active-horiz" } ; vertical divider of horizontal layout
+                                     :style {:cursor "ns-resize"
+                                             :height 5}}]}}})))
 
 (defnc form []
   ($ Stack {:direction "column"
@@ -66,9 +33,11 @@
                     :padding         "2px 0 2px 30px"
                     :noWrap  false}
         "RADmapper")
-     ($ LeftRightShare
-        {:left  ($ TextField {:multiline true :placeholder "true left"})
-         :right ($ TextField {:multiline true :placeholder "true right"})})))
+     ($ ShareLeftRight
+        {:left  ($ TextField {:multiline true :placeholder "share left/right left"})
+         :right ($ ShareUpDown
+                   {:up   ($ TextField {:multiline true :placeholder "(right) share up/down up"})
+                    :down ($ TextField {:multiline true :placeholder "(right) share up/down down"})})})))
 
 (defnc app []
   {:helix/features {:check-invalid-hooks-usage true}}
